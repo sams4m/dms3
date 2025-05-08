a7eab026571f6237f41897a30c36ee9f0fa755f3
Sam <kyj.samantha@gmail.com>
2025-05-08 17:25:11 +1000

    style

--- components/HomeButton.tsx
+++ components/HomeButton.tsx
@@ -2,7 +2,7 @@ export default function HomeButton() {
   return (
     <a
       href="/blog"
-      class="rounded-md p-2 bg-transparent border border-primary text-foreground hover:var(--color-muted-foreground)"
+      class="rounded-md p-2 bg-transparent border border-primary text-foreground hover:#7d9fc0"
       aria-label="Back to index"
     >
       ← back
